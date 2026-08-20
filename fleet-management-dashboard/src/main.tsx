9757fdbcafd224be2260c91623acbb4a17b060eb
syeda-hira-batool <ic.shirabatool09@gmail.com>
2026-08-20 21:00:23 +0500

    removed the strict mode

--- fleet-management-dashboard/src/main.tsx
+++ fleet-management-dashboard/src/main.tsx
@@ -1,13 +1,14 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from "react";
 
 import "./index.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
-  
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  
+  </StrictMode>
 );
